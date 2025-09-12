ba172610232d70df474303ca33c3165d841432d2
Paulo Sandim <pauloedusandim@gmail.com>
2025-09-12 16:49:03 -0300

    configuracao padrao do cypress

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "https://front.serverest.dev",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    pageLoadTimeout: 120000,
+    chromeWebSecurity: false,
+  },
+});
