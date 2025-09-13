a4c21ce99be68fbd46c2d44be671fef093dc1892
Paulo Sandim <pauloedusandim@gmail.com>
2025-09-13 17:42:18 -0300

    deixando teste de cadastro independente com a biblioteca Faker

--- cypress/e2e/ui/cadastro-produto.cy.js
+++ cypress/e2e/ui/cadastro-produto.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 const url = Cypress.env("FRONT_URL")
 
 describe('Testes para Cadastro de Produtos', () => {
@@ -8,15 +10,20 @@ describe('Testes para Cadastro de Produtos', () => {
   })
 
   it('Validar o cadastro de um produto com sucesso', () => {
-    cy.get('[data-testid="cadastrarProdutos"]').click()
+    const nomeProduto = faker.commerce.productName()
+    const precoProduto = faker.commerce.price({ min: 1, max: 1000, dec: 0 })
+    const descricaoProduto = faker.commerce.productDescription()
+    const quantidadeProduto = faker.number.int({ min: 10, max: 100 })
 
-    cy.get('[data-testid="nome"]').type('Pack Coronita')
-    cy.get('[data-testid="preco"]').type('35')
-    cy.get('[data-testid="descricao"]').type('Cerveja 210 ml, leve e refrescante')
-    cy.get('[data-testid="quantity"]').type('80')
+    cy.get('[data-testid="cadastrarProdutos"]').click()
+    cy.get('[data-testid="nome"]').type(nomeProduto)
+    cy.get('[data-testid="preco"]').type(precoProduto)
+    cy.get('[data-testid="descricao"]').type(descricaoProduto)
+    cy.get('[data-testid="quantity"]').type(quantidadeProduto)
     cy.get('[data-testid="imagem"]').selectFile('cypress/fixtures/pack-coronita.png')
     cy.get('[data-testid="cadastarProdutos"]').click()
 
     cy.contains('Lista dos Produtos').should('be.visible')
+    cy.contains('tr', nomeProduto).should('exist')
   })
 })
